d91a279a9abca272be6bff791417f24d15b42222
ISWAD <iswad.brand@gmail.com>
2020-11-09 11:22:27 -0500

    login server is created

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+const database = {
+	users: [
+    	{
+        	id: '1',
+        	firstName: "Mohammad",
+        	lastName: "Mohajer",
+        	email: "mmmohajer70@gmail.com",
+        	password: "123456",
+        	comments: 0,
+        	joined: new Date()
+        },
+        {
+        	id: '2',
+        	firstName: "Amin",
+            lastName: "Nouri",
+            email: "am_nouri@gmail.com",
+            password: "78910",
+            comments: 0,
+            joined: new Date()
+        }
+    ]
+};
+
 app.get('/', (req, res) => {
 	res.send("This is working!")
+})
+
+app.post('/login', (req, res) => {
+	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+		res.json('Success, user logged in');
+	} else {
+		res.status(400).json("Error logging in");
+	}
 })
 
 app.listen(process.env.PORT || 4000, () => {
